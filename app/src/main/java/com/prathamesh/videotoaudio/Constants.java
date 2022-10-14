e5a19a479b6f79aa66c38771b1de3722059a636c
Prathamesh Adate <prathameshadate05@gmail.com>
2022-10-14 10:01:04 +0530

    Home Activity Added

---
+++ app/src/main/java/com/prathamesh/videotoaudio/Constants.java
@@ -0,0 +1,5 @@
+package com.prathamesh.videotoaudio;
+
+public class Constants {
+    public static final int SPLASH = 3000;
+}
--- app/src/main/java/com/prathamesh/videotoaudio/Splash.java
+++ app/src/main/java/com/prathamesh/videotoaudio/Splash.java
@@ -2,7 +2,9 @@ package com.prathamesh.videotoaudio;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.widget.ImageView;
 
 import com.bumptech.glide.Glide;
@@ -20,11 +22,19 @@ public class Splash extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
 
-
         Objects.requireNonNull(getSupportActionBar()).hide();
 
         splashBack = findViewById(R.id.IV_Splash);
         Glide.with(this).load(R.drawable.splash_background).into(splashBack);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(Splash.this,Home.class);
+                startActivity(intent);
+                finish();   
+            }
+        },Constants.SPLASH);
 
     }
 }
