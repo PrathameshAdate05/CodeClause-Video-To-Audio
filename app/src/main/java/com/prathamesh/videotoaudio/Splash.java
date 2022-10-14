15d612568891e11e1bb1094755887875a385ecab
Prathamesh Adate <prathameshadate05@gmail.com>
2022-10-14 09:51:14 +0530

    Confilts Resolve

--- app/src/main/java/com/prathamesh/videotoaudio/Splash.java
+++ app/src/main/java/com/prathamesh/videotoaudio/Splash.java
@@ -1,8 +1,6 @@
 package com.prathamesh.videotoaudio;
 
 import androidx.appcompat.app.AppCompatActivity;
-
-<<<<<<< HEAD
 import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.widget.ImageView;
@@ -16,24 +14,17 @@ public class Splash extends AppCompatActivity {
     ImageView splashBack;
 
     @SuppressLint("MissingInflatedId")
-=======
-import android.os.Bundle;
 
-public class Splash extends AppCompatActivity {
-
->>>>>>> 1f02d215357fbecd577598349e1f92c072207ea8
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
-<<<<<<< HEAD
+
 
         Objects.requireNonNull(getSupportActionBar()).hide();
 
         splashBack = findViewById(R.id.IV_Splash);
         Glide.with(this).load(R.drawable.splash_background).into(splashBack);
 
-=======
->>>>>>> 1f02d215357fbecd577598349e1f92c072207ea8
     }
 }
